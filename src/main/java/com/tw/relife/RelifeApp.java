2eabc842791400ada4eb735f82e1622e15371933
Yanping Liu <yanpliu@thoughtworks.com>
2018-09-27 10:43:00 +0800

    [Yanping Liu][Story1-AC1]return response successfully

--- src/main/java/com/tw/relife/RelifeApp.java
+++ src/main/java/com/tw/relife/RelifeApp.java
@@ -1,18 +1,20 @@
 package com.tw.relife;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class RelifeApp implements RelifeAppHandler {
     private final RelifeAppHandler handler;
 
     public RelifeApp(RelifeAppHandler handler) {
         // TODO: You can start here
-        throw new NotImplementedException();
+        if (handler == null) {
+            throw new IllegalArgumentException();
+        }
+        this.handler = handler;
     }
 
     @Override
     public RelifeResponse process(RelifeRequest request) {
         // TODO: You can start here
-        throw new NotImplementedException();
+        RelifeResponse response = this.handler.process(request);
+        return response;
     }
 }
